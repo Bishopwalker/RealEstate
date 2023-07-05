72de5c7123f7f7c32c0b337cd81e4b8f91cf2187
Bishop Walker <bishop.walker@sbd2.com>
2023-07-05 10:52:03 -0400

    now header is set for medea quiries

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -4,17 +4,20 @@ import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { makeStyles } from '@mui/styles';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 import SearchIcon from '@mui/icons-material/Search';
-import MailIcon from "@mui/icons-material/Mail";
-import TextField from '@mui/material/TextField'; // Import TextField
-import IconButton from "@mui/material/IconButton";
+import MailIcon from '@mui/icons-material/Mail';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Drawer from '@mui/material/Drawer';
+import MenuIcon from '@mui/icons-material/Menu';
 
-import {Navigate} from "react-router-dom";
-
-import {useEffect, useState} from "react";
-import axios from "axios";
-import {SearchOutlined} from "@mui/icons-material"; // Import useState
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { SearchOutlined } from '@mui/icons-material';
 
 const useStyles = makeStyles({
     button: {
@@ -33,10 +36,11 @@ const useStyles = makeStyles({
 const Header = () => {
     const classes = useStyles();
 
-    const [searchInput, setSearchInput] = useState(''); // State for search input
-    const [propertyMLS, setPropertyMLS] = useState(null); // State for property MLS number
- const [property, setProperty] = useState({});
-    const [showSearchField, setShowSearchField] = useState(false); // State for search field visibility
+    const [searchInput, setSearchInput] = useState('');
+    const [propertyMLS, setPropertyMLS] = useState(null);
+    const [property, setProperty] = useState({});
+    const [showSearchField, setShowSearchField] = useState(false);
+    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     const navItems = [
         { name: 'My Story', url: '/about' },
@@ -46,33 +50,24 @@ const Header = () => {
         { name: 'Reviews', url: '/review' },
         { name: 'Contact Us', url: '/contact' },
     ];
-console.log(searchInput)
+
     const handleSearchInputChange = (event) => {
         setSearchInput(event.target.value);
-
-        console.log(searchInput)
-
-
     };
-
 
     const handleSearchIconClick = () => {
-        setShowSearchField(!showSearchField); // Toggle search field visibility
+        setShowSearchField(!showSearchField);
     };
-
-
-
 
     const fetchProduct = async (e) => {
         e.preventDefault();
 
         if (!searchInput) {
             return;
-    }
+        }
+
         try {
-            const response = await axios.get(
-                `http://localhost:5000/api/property-by-mls-id/${searchInput}`
-            );
+            const response = await axios.get(`http://localhost:5000/api/property-by-mls-id/${searchInput}`);
             console.log(response.data);
             setProperty(response.data);
         } catch (error) {
@@ -80,57 +75,101 @@ console.log(searchInput)
         }
     };
 
-console.log(property)
     if (property.data) {
         return <Navigate to="/property-details" state={{ property: property }} />;
     }
 
-
     return (
-        <AppBar position="sticky" sx={{ backgroundColor: 'black' }}>
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                <Typography
-                    variant="h4"
-                    component="span"
-                    sx={{ fontFamily: "'Great Vibes', cursive", color: 'white' }}
-                >
-                    <Link to="/" className={classes.button}>
-                        David Fine
-                    </Link>
-                </Typography>
-                <Box sx={{ display: 'flex', justifyContent: 'space-between', flexGrow: 1, mx: 8 }}>
-                    {navItems.map((item, index) => (
-                        <Button
-                            key={index}
-                            component={Link}
-                            to={item.url}
-                            className={classes.button}
-                        >
-                            {item.name}
-                        </Button>
-                    ))}
-                    <form onSubmit={(e)=>fetchProduct(e)}>
-                    <TextField
+        <Box>
+            <AppBar position="sticky" sx={{ backgroundColor: 'black' }}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography
+                        variant="h4"
+                        component="span"
+                        sx={{ fontFamily: "'Great Vibes', cursive", color: 'white' }}
+                    >
+                        <Link to="/" className={classes.button}>
+                            David Fine
+                        </Link>
+                    </Typography>
 
-                         id="standard-bare"
-                        variant="outlined"
-                        onChange={handleSearchInputChange}
-                         sx={{backgroundColor:'white', color:'red'}}
-                        placeholder="Enter MLS #"
-                        InputProps={{
-                            endAdornment: (
-                                <IconButton>
-                                    <SearchOutlined />
-                                </IconButton>
-                            ),
+                    {/* Mobile Menu */}
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        onClick={() => setMobileMenuOpen(true)}
+                        sx={{ display: { md: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+
+                    {/* Desktop Menu */}
+                    <Box
+                        sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            flexGrow: 1,
+                            mx: 8,
+                            display: { xs: 'none', md: 'flex' },
                         }}
-                    />
-                    </form>
-                </Box>
+                    >
+                        {navItems.map((item, index) => (
+                            <Button
+                                key={index}
+                                component={Link}
+                                to={item.url}
+                                className={classes.button}
+                            >
+                                {item.name}
+                            </Button>
+                        ))}
 
-                <MailIcon />
-            </Toolbar>
-        </AppBar>
+                        <form onSubmit={fetchProduct}>
+                            <TextField
+                                id="standard-bare"
+                                variant="outlined"
+                                onChange={handleSearchInputChange}
+                                sx={{ backgroundColor: 'white', color: 'red' }}
+                                placeholder="Enter MLS #"
+                                InputProps={{
+                                    endAdornment: (
+                                        <IconButton>
+                                            <SearchOutlined />
+                                        </IconButton>
+                                    ),
+                                }}
+                            />
+                        </form>
+                    </Box>
+
+                    <MailIcon />
+                </Toolbar>
+            </AppBar>
+
+            {/* Mobile Drawer */}
+            <Drawer
+                anchor="left"
+                open={mobileMenuOpen}
+                onClose={() => setMobileMenuOpen(false)}
+            >
+                <Box sx={{ width: 250 }} onClick={() => setMobileMenuOpen(false)}>
+                    <List>
+                        {navItems.map((item, index) => (
+                            <ListItem
+                                key={index}
+                                button
+                                component={Link}
+                                to={item.url}
+                                className={classes.button}
+                            >
+                                <ListItemText primary={item.name} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+            </Drawer>
+        </Box>
     );
 };
 
