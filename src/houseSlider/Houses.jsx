f40a9b8fa7a17238f3cd8c49d26949f535dbed34
Bishop Walker <bishop.walker@sbd2.com>
2023-07-28 00:21:41 -0400

    fixup! search by city and state

--- src/houseSlider/Houses.jsx
+++ src/houseSlider/Houses.jsx
@@ -1,12 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 //@ts-ignore
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import {Box} from "@mui/material";
+import {Box, Card, CardContent, CardMedia} from "@mui/material";
 import "./style.css";
 import {useDispatch, useSelector} from "react-redux";
 import {fetchHousesbyMLS} from "../redux/agentListingsSlice.js";
+import Typography from "@mui/material/Typography";
 
 const mlsIds = [112844, 113591, 113740, 113741, 113935];
 function Service( ) {
@@ -18,7 +19,7 @@ function Service( ) {
         dispatch(fetchHousesbyMLS(mlsIds));
     }, [dispatch]);
 
-    console.log(houses);
+    console.log([houses[0].data.results[0]]   );
 
 
     const settings = {
@@ -62,26 +63,48 @@ function Service( ) {
 
 
     return (
-        <Box>
+        <Box  >
             <Slider {...settings}>
-                {  houses.data.results.map((result, resultIndex) => (
-                    <div key={resultIndex} className="card">
-                        <div className="card-top">
-                            <img src={result.primary_photo.href} alt="House" />
-                            <h3>{`${result.location.address.line}, ${result.location.address.city}, ${result.location.address.state_code} ${result.location.address.postal_code}`}</h3>
-                            <p>{result.branding[0].name}</p>
-                            <p>Baths: {result.description.baths}</p>
-                            <p>Full Baths: {result.description.baths_full}</p>
-                            <p>Beds: {result.description.beds}</p>
-                            <p>Garage: {result.description.garage}</p>
-                            <p>Lot Sqft: {result.description.lot_sqft}</p>
-                            <p>Sqft: {result.description.sqft}</p>
-                            <p>Stories: {result.description.stories}</p>
-                            <p>Type: {result.description.type}</p>
-                            <p>Year Built: {result.description.year_built}</p>
-                            <p>Last Update Date: {result.last_update_date}</p>
-                        </div>
-                    </div>
+                {houses[0].data && [houses[0].data.results[0]].map((property, index) => (
+                    <Card key={index}>
+                        <CardMedia
+                            component="img"
+                            alt="Property Image"
+                            height="140"
+                            image={property.primary_photo.href}
+                        />
+                        <CardContent>
+                            <Typography variant="h5" component="div">
+                                {property.location.address.line}, {property.location.address.city}, {property.location.address.state_code} {property.location.address.postal_code}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Price: ${property.list_price}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Status: {property.status}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Description: {property.description.type}
+                            </Typography>
+                            {property.branding.map((brand, i) => (
+                                <Typography key={i} variant="body2" color="text.secondary">
+                                    Branding: {brand.name}
+                                </Typography>
+                            ))}
+                            <Typography variant="body2" color="text.secondary">
+                                Community: {property.community || "N/A"}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Baths: {property.description.baths || "N/A"}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Lot Sqft: {property.description.lot_sqft || "N/A"}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                New Listing: {property.flags.is_new_listing ? "Yes" : "No"}
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 ))}
             </Slider>
         </Box>
@@ -93,7 +116,11 @@ function Service( ) {
 Service.defaultProps = {
     houses: {
         data: {
-            results: [],
+            results: [{
+                0: {
+
+                }
+            }],
         },
     },
 };
